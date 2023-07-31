e327debbddf4f2c491e9f616b2af6336138114b0
Colby <colbyx20@gmail.com>
2023-07-31 15:33:21 -0400

    added server

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"text/template"
 
@@ -11,10 +13,25 @@ var homeTemplate *template.Template
 
 func renderHTMLTemplate(w http.ResponseWriter, tmpl *template.Template, data interface{}) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	tmpl.Execute(w, data)
 }
 
 func HomeRender(w http.ResponseWriter, r *http.Request) {
+
+	data := struct{}{}
+
+	renderHTMLTemplate(w, homeTemplate, data)
+}
+
+func EnterEmail(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Failed to parse Data", http.StatusInternalServerError)
+	}
+
+	email := r.FormValue("mlist")
+	fmt.Println(email)
 
 	data := struct{}{}
 
@@ -28,7 +45,18 @@ func main() {
 	homeTemplate = template.Must(template.ParseFiles("src/index.html"))
 	router.Use(mux.CORSMethodMiddleware(router))
 
+	router.Use(loggingMiddleware)
 	router.HandleFunc("/", HomeRender).Methods("GET")
+	router.HandleFunc("/emailSignUp", EnterEmail)
 
 	http.ListenAndServe(":4000", router)
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		log.Println(r.RequestURI)
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
 }
